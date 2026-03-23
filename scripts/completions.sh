0db813a2cf0b591e08e7e2f9b407ad076e9aa215
StefanBethge <161315255+StefanBethge@users.noreply.github.com>
2026-03-23 18:38:35 +0100

    feat: automatically install completions #32 (#46)
    Closes #32 
    
    
    So i implementing this feature. I make an Local test with go releaser.

---
+++ scripts/completions.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+cd "$(dirname "$0")/.."
+rm -rf completions
+mkdir completions
+go run ./cmd/odoo-work-cli completion bash > "completions/odoo-work-cli.bash"
+go run ./cmd/odoo-work-cli completion zsh > "completions/_odoo-work-cli"
+go run ./cmd/odoo-work-cli completion fish > "completions/odoo-work-cli.fish"
